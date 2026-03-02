06a020d27e012ca8ef9e8548f3121ba2218ce7ee
vishnuu45 <vardhanks45@gmail.com>
2026-03-02 19:13:26 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,1 +1,19 @@
+package com.example;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testGradeA() {
+        App app = new App();
+        assertEquals("A", app.calculateGrade(95));
+    }
+
+    @Test
+    public void testGradeFail() {
+        App app = new App();
+        assertEquals("Fail", app.calculateGrade(30));
+    }
+}
